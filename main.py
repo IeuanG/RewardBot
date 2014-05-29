445daf84a93b19da4f3d849fa1f01dbfda762def
IeuanG <ieuangriffiths1230@gmail.com>
2014-05-29 16:25:14 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,42 +2,30 @@ import praw
 import re
 import time
 
-username = 'much_help'
-password = 'datfakepassword'
+username = 'muchtipshibe'
+password = 'Parciau'
 
 r = praw.Reddit('you_get_doge')
-r.config._ssl_url = None
 r.login(username,password)
 
 already_done = []
 
-def msg(karma):
-	msg = '''
-Thanks for making a good post to /r/dogecoin, looks like it's gaining some upvotes!
-
-Here, exchange those votes for Doge: +/u/dogetipbot %s doge
-
-
----
-
-Beep Beep! Im a bot, if you have any problems, you can message me, /u/ieuang, or /u/addm3plz.
-''' % str(karma)
-	return msg
 
 print 'starting...'
 
 while True:
+
 	subreddit = r.get_subreddit('dogecoin')
-	for submission in subreddit.get_new(limit=10):
-		print 'Checking...'
+
+	for submission in subreddit.get_new(limit=20):
+
+		print ('Checking...', submission.id)
 		if submission.id not in already_done:
-			karma = submission.ups - submission.downs
-			if float(karma) >= float(5):
-				submission.upvote()
-				print "upvoted!"
-				submission.add_comment(msg(karma))
-				print 'replied!'
-				already_done.append(submission.id)
-				
-	print 'sleeping'
-	time.sleep(200)
+					submission.upvote()
+					print ("upvoted! ", submission.id)
+					submission.add_comment('To the mooooon! +/u/dogetipbot 4 doge')
+					print ('replied! ', submission.id)
+					already_done.append(submission.id)
+
+	print ('sleeping for 10 seconds')
+	time.sleep(10)
